3d46994e1d3a1cea2dd588ff038143d6b7c99e51
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-10-30 17:53:28 +0100

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: 'Luomy', lastName: 'EQua', email: 'luomyequa@justenoughbank.com'}
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                type='greeting'
+                title='Welcome'
+                user={loggedIn?.firstName || 'Guest'}
+                subtext='Access and manage your account and transactions efficiently' />
+
+                <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={10000.20}  />
+            
+            </header>
+            RECENT TRANSACTIONS
+        </div>
+        <RightSideBar user={loggedIn} transactions={[]} banks={[{currentBalance: 12000},{currentBalance: 1250}]} />
+    </section>
+    
   )
 }
 
